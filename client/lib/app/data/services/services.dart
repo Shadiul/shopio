48499926fcb408e63ba4a16454559a6b454dbaf0
Shadiul Huda <shadiul.71@gmail.com>
2021-11-14 06:56:00 +0600

    Add firebase core and google service json

--- client/lib/app/data/services/services.dart
+++ client/lib/app/data/services/services.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../utils/log_colored.dart';
@@ -7,6 +9,8 @@ export 'auth_service.dart';
 
 Future<void> initServices() async {
   logColored('📦 Starting services', color: LogColor.White);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await Get.putAsync(() => AuthService().init());
   logColored('✔️ All Service Started', color: LogColor.White);
 }
