16d5dc00e862e31fe7540a9c5bdfcb72ca04edcf
Shadiul Huda <shadiul.71@gmail.com>
2021-11-16 00:57:32 +0600

    🚀 Add Fake categories

--- client/lib/app/modules/explore/explore_controller.dart
+++ client/lib/app/modules/explore/explore_controller.dart
@@ -1,3 +1,45 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-class ExploreController extends GetxController {}
+import '../../data/enums.dart';
+import '../../data/models/category_model.dart';
+import '../../utils/log_colored.dart';
+
+class ExploreController extends GetxController {
+  Rx<UiState> categoriesState = Rx(UiState.idle);
+
+  @override
+  Future<void> onReady() async {
+    await getCategories();
+    super.onReady();
+  }
+
+  RxList<CategoryModel> categories = RxList<CategoryModel>();
+  Future<void> getCategories() async {
+    logColored('📦 Loading fake categories from asset', color: LogColor.white);
+    categoriesState.value = UiState.loading;
+
+    try {
+      final categoryBundleData = await rootBundle.loadString(
+        'assets/json/fake_categories.json',
+      );
+      final jsonData = jsonDecode(categoryBundleData);
+      categories.value = List.from((jsonData['data'] as List).map(
+        (e) => CategoryModel.fromJson(e),
+      ));
+      logColored(
+        '📦 Success loading fake categories from asset',
+        color: LogColor.green,
+      );
+    } catch (e) {
+      log(e.toString());
+      categoriesState.value = UiState.error;
+      return;
+    }
+
+    categoriesState.value = UiState.success;
+  }
+}
--- client/lib/app/modules/explore/widgets/category_mini.dart
+++ client/lib/app/modules/explore/widgets/category_mini.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../data/enums.dart';
+import '../../../data/models/category_model.dart';
 import '../explore_controller.dart';
 
 class CategoryMiniWidget extends GetWidget<ExploreController> {
@@ -36,33 +38,51 @@ class CategoryMiniWidget extends GetWidget<ExploreController> {
         ),
         SizedBox(
           height: 120.0,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 8.0,
-            ),
-            itemCount: 10,
-            itemBuilder: (context, index) => SizedBox(
-              width: 64,
-              child: Column(
-                children: [
-                  const CircleAvatar(
-                    radius: 32.0,
-                    child: Icon(Icons.shop),
-                  ),
-                  const SizedBox(height: 4.0),
-                  Expanded(
-                    child: Text(
-                      'Category name',
-                      textAlign: TextAlign.center,
-                      style: _textTheme.subtitle2,
+          child: Obx(
+            () => controller.categoriesState.value == UiState.loading
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
                     ),
-                  )
-                ],
-              ),
-            ),
-            separatorBuilder: (context, index) => const SizedBox(width: 8.0),
+                    itemCount: controller.categories.length,
+                    itemBuilder: (context, index) {
+                      CategoryModel category = controller.categories[index];
+
+                      return SizedBox(
+                        width: 64,
+                        child: Column(
+                          children: [
+                            MaterialButton(
+                              onPressed: () {},
+                              shape: const CircleBorder(),
+                              color: _themeData.colorScheme.primary,
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              padding: const EdgeInsets.all(16.0),
+                              child: Image.asset(
+                                'assets/icons/${category.icon}.png',
+                                fit: BoxFit.contain,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 4.0),
+                            Expanded(
+                              child: Text(
+                                category.name,
+                                textAlign: TextAlign.center,
+                                style: _textTheme.subtitle2,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(width: 8.0),
+                  ),
           ),
         ),
       ],
